8be6cd9b02094db3c7a3edef84a1968d771c0f14
vismayarajeevan <vismayarajeevan15@gmail.com>
2024-12-10 21:18:39 +0530

    make resposive

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const [bgColor, setBgColor] =useState("light")
       
       <div style={{width:"100%", height:"100vh", background:bgColor}} className='d-flex justify-content-center align-items-center'>
         <div className='p-5 rounded'>
-          <h1 className='text-center fw-bold'>Background Color Changer</h1>
+          <h1 className='head text-center fw-bold'>Background Color Changer</h1>
           <div className='p-5 d-flex justify-content-center'>
             <button onClick={()=>setBgColor("black")} className='button rounded p-2 m-2 bg-dark text-light'>Black</button>
             <button onClick={()=>setBgColor("red")} className='button rounded p-2 m-2 bg-danger text-light'>Red</button>
